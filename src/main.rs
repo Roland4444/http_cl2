e625f11075001108cbd93333b0a0e8a04cda314e
Roland4444 <turbopollux@rambler.ru>
2026-01-20 16:23:39 +0400

    dding files`:w:wq

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ fn read_bytes(filename: &str)-> Vec<u8> {
 
 
 const TARGET_ADDRESS: &str = "http://localhost:11111/custom";
+const WEBHOOK_FILENAME: &str = "webhook";
+const WEBHOOK_FILENAME_TEST: &str = "webhook_test";
 
 fn read_lines(filename: &str) -> Vec<String>{
     let bytes = read_bytes(filename);
@@ -34,6 +36,13 @@ pub fn update_second_name(second_name: &str, ID: i32) -> String{
     "SUCCESS".to_string()
 }
 
+
+pub fn update_param_(param_name: &str, value: &str, ID: i32) -> String{
+
+
+    "SUCCESS".to_string()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
@@ -55,6 +64,16 @@ pub fn bad_add(a: i32, b: i32) -> i32{
     a - b
 }
 
+
+fn get_webhook() -> String{
+    get_webhook_(WEBHOOK_FILENAME)
+}
+
+fn get_webhook_(filename: &str) -> String{
+    let vec = read_lines(filename);
+    let elem = vec[0].clone();
+    elem
+}
 
 #[cfg(test)]
 mod tests {
@@ -78,6 +97,14 @@ mod tests {
         let vect = read_lines(filename);
         let line0 = vect[0].clone();
         assert_eq!(etalon, line0);
+    }
+
+    #[test]
+    fn test_read_webbhook(){
+        let test_webhook = get_webhook_(WEBHOOK_FILENAME_TEST);
+        let etalon = "http://google.com";
+        assert_eq!(etalon, test_webhook);
+
     }
 }
 
