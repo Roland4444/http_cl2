84ab090d8f6de464f0da011ad9edc0f756e6b493
Roland4444 <turbopollux@rambler.ru>
2026-08-26 17:33:46 +0400

    need tets

--- src/checker.rs
+++ src/checker.rs
@@ -8,3 +8,14 @@ pub fn get_work_pos(input: String) -> String {
         EFES.to_string()
     }
 }
+
+
+pub fn get_f_(input: String) -> String {
+    if let Some(pos) = input.find('=') {
+        let after = &input[pos + 1..];
+        return after.split_whitespace().next().unwrap_or("").to_string();
+    } else
+    {
+        "" .to_string()
+    }
+}
